3c0240f4d590acb20e4c0e0511448d7f725069c4
Korkogor <korkogor@gmail.com>
2025-05-18 11:50:10 +0300

    Add project files.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Создан новый класс
+namespace PickRandomCards
+{
+    internal class CardPicker
+    {
+        //Создан метод для набора карт
+        public static string[] PickSomeCards (int numberOfCards)
+        {
+            // Создан массив строк pickedCards, который набирается из переменной int numberOfCards
+            string[] pickedCards = new string[numberOfCards];
+            // Цикл for показывает, что в массив pickedCards будут добавляться карты, до того кол-ва, которое указано переменной numberOfCards
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();            
+            }
+            return pickedCards;
+        }
+
+        private static string RandomValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string RandomSuit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
