3d6f354ac063dcc60dcf12951965ff4040f18f88
Korkogor <korkogor@gmail.com>
2025-05-24 20:03:42 +0300

    Added the latest comments

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -1,23 +1,28 @@
 ﻿using PickRandomCards;
 
-static void Main(string[] args)
-{
-    Console.Write("Enter the number of cards to pick: ");
-    string line = Console.ReadLine();
-
-    if(int.TryParse(line, out int numberOfCards))
+// класс Программа
+internal class Program
+{//Метод main, который запрашивает у пользователя..
+    static void Main(string[] args)
     {
-        // этот блок выполняется в тому случае, если строка МОЖЕТ БЫТЬ преобразована в int
-        // значение, сохраняемое в новой переменной, называется numberOfCards
-        foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+        Console.Write("Enter the number of cards to pick: "); // запросить у пользователя количество карт
+        string line = Console.ReadLine(); // запись с консоли в переменную line
+
+        if (int.TryParse(line, out int numberOfCards))
         {
+            // этот блок выполняется в тому случае, если строка МОЖЕТ БЫТЬ преобразована в int
+            // значение, сохраняемое в новой переменной, называется numberOfCards
+            foreach (string card in CardPicker.PickSomeCards(numberOfCards)) // Данный цикл выполняет Console.WriteLine(card) для каждого
+                //элемента массива, возвращённого PickSomeCard
+            {
+                Console.WriteLine(card);
+            }
 
         }
-
-    }
-    else
-    {
-        //этот блок выполняется, если строка НЕ МОЖЕТ БЫТЬ преобразована в int
-        Console.WriteLine("Введённое значение не подходит. Пожалуйста, введите число");
+        else
+        {
+            //этот блок выполняется, если строка НЕ МОЖЕТ БЫТЬ преобразована в int
+            Console.WriteLine("Введённое значение не подходит. Пожалуйста, введите число");
+        }
     }
 }
